87f789263f2adbf9d2ae990664c66ae8047d44dc
melon <melonnelise@gmail.com>
2019-06-25 22:58:50 -0700

    now detects multiple keypresses (allows for diagonal movement)

--- src/server/js/Player.js
+++ src/server/js/Player.js
@@ -1,32 +1,18 @@
-const schema = require('@colyseus/schema');
-const type = schema.type;
 const Entity = require('./Entity').Entity;
+const Key = require('./Key').Key;
 
 class Player extends Entity {
   constructor (x = 0, y = 0) {
     super(x, y);
 
     // these may be remapped later
-    this.key_up = 87; // W
-    this.key_left = 65; // A
-    this.key_down = 83; // S
-    this.key_right = 68; // D
-    this.key_action = 32; // SPACE
-  }
-
-  printKeys () {
-    console.log('up: ' + this.key_up);
-    console.log('left: ' + this.key_left);
-    console.log('down: ' + this.key_down);
-    console.log('right: ' + this.key_right);
+    this.keyUp = new Key(87); // W
+    this.keyLeft = new Key(65); // A
+    this.keyDown = new Key(83); // S
+    this.keyRight = new Key(68); // D
+    this.keyAction = new Key(32); // SPACE
   }
 }
-
-type('uint8')(Entity.prototype, 'key_up');
-type('uint8')(Entity.prototype, 'key_left');
-type('uint8')(Entity.prototype, 'key_down');
-type('uint8')(Entity.prototype, 'key_right');
-type('uint8')(Entity.prototype, 'key_action');
 
 // other possible properties: weight, sprite, collision flag, etc
 
