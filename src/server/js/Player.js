10e1896bc8f84a1b43624f50930ae41c041c25cb
melon <melonnelise@gmail.com>
2019-06-29 12:03:29 -0700

    added floorsensor to players

--- src/server/js/Player.js
+++ src/server/js/Player.js
@@ -4,9 +4,10 @@ const Key = require('./Key').Key;
 
 class Player extends Entity {
   constructor (x, y) {
-    super(Matter.Bodies.rectangle(x, y, 100, 150, {inertia: Infinity, inverseInertia: 0, friction: 0}));
+    var playerBody = Matter.Bodies.rectangle(x, y, 100, 150, {inertia: Infinity, inverseInertia: 0, friction: 0});
+    var floorSensor = Matter.Bodies.circle(x, y + 10, 2,{density:0, friction:0, isSensor: true});
 
-
+    super(Matter.Body.create({parts: [playerBody, floorSensor], friction:0}));
     this.speed = 5;
     // these may be remapped later
     this.keyUp = new Key(87); // W
